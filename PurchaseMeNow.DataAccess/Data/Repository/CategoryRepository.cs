d4d829b35070f7043b36ac4dfd4a0872ae2947da
shihab <echoshihab@gmail.com>
2020-03-30 23:37:52 -0400

    add Category repository interface and implement

---
+++ PurchaseMeNow.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
+using PurchaseMeNow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PurchaseMeNow.DataAccess.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+
+                _db.SaveChanges();
+
+            }
+         
+        }
+    }
+}
