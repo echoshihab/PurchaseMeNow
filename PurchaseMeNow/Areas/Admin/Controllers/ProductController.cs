84e97392001a84250d641d43ff0b4ce22c38e6eb
shihab <echoshihab@gmail.com>
2020-04-04 17:30:57 -0400

    add product view model

---
+++ PurchaseMeNow/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
+using PurchaseMeNow.Models;
+
+namespace PurchaseMeNow.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? Id)
+        {
+            Product product = new Product();
+
+            //create new
+            if (Id == null)
+            {
+                return View(product);
+            }
+
+            //edit existing
+            product = _unitOfWork.Product.Get(Id.GetValueOrDefault()); ;
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(product);
+                }
+                _unitOfWork.Save();
+                RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        #region Api CAlls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll();
+
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(Id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { sucess = false, message = "Error While Deleting" });
+            }
+
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+
+        #endregion
+    }
+
+}
