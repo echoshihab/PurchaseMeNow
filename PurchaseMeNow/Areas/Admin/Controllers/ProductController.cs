53733738bcc12eacc5b00d50892d0bced368d330
shihab <echoshihab@gmail.com>
2020-04-04 17:52:53 -0400

    add product controller

--- PurchaseMeNow/Areas/Admin/Controllers/ProductController.cs
+++ PurchaseMeNow/Areas/Admin/Controllers/ProductController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
 using PurchaseMeNow.Models;
+using PurchaseMeNow.Models.ViewModels;
 
 namespace PurchaseMeNow.Areas.Admin.Controllers
 {
@@ -23,50 +25,64 @@ namespace PurchaseMeNow.Areas.Admin.Controllers
 
         public IActionResult Upsert(int? Id)
         {
-            Product product = new Product();
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                }),
+                DepartmentList = _unitOfWork.Department.GetAll().Select(d => new SelectListItem
+                {
+                    Text = d.Name,
+                    Value = d.Id.ToString()
+                })
+
+            };
 
             //create new
             if (Id == null)
             {
-                return View(product);
+                return View(productVM);
             }
 
             //edit existing
-            product = _unitOfWork.Product.Get(Id.GetValueOrDefault()); ;
+            productVM.Product = _unitOfWork.Product.Get(Id.GetValueOrDefault()); ;
 
-            if (product == null)
+            if (productVM.Product == null)
             {
                 return NotFound();
             }
 
-            return View(product);
+            return View(productVM);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                if (product.Id == 0)
-                {
-                    _unitOfWork.Product.Add(product);
-                }
-                else
-                {
-                    _unitOfWork.Product.Update(product);
-                }
-                _unitOfWork.Save();
-                RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (product.Id == 0)
+        //        {
+        //            _unitOfWork.Product.Add(product);
+        //        }
+        //        else
+        //        {
+        //            _unitOfWork.Product.Update(product);
+        //        }
+        //        _unitOfWork.Save();
+        //        RedirectToAction(nameof(Index));
+        //    }
+        //    return View(product);
+        //}
 
         #region Api CAlls
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Product.GetAll();
+            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,Department");
 
             return Json(new { data = allObj });
         }
