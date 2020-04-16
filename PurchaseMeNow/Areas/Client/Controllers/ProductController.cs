e89d1ad9df3477c27bf39904782e5e0c7a9eb4c8
shihab <echoshihab@gmail.com>
2020-04-15 20:32:46 -0400

    add product controller for client + index view

---
+++ PurchaseMeNow/Areas/Client/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
+
+namespace PurchaseMeNow.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController( IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,Department");
+
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+
+
+}
+
---
+++ PurchaseMeNow/wwwroot/js/product_client.js
@@ -0,0 +1,61 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Client/Product/GetAll"
+        },
+        "language": {
+            "infoEmpty": "Nothing found!",
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "description", "width": "25%" },
+            { "data": "category.name", "width": "20%" },
+            { "data": "department.name", "width": "20%" },
+            {
+                "data": "id", "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Client/Product/Order/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-shopping-cart"></i>
+                                </a>
+                          
+                        </div>
+`
+                }, "width": "40%"
+            }
+        ]
+
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "All deletions are permanent",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
