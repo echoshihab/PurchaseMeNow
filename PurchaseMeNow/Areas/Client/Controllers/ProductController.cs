4a96911e301d5161968e61f6bf8f4339434bfd79
shihab <echoshihab@gmail.com>
2020-04-18 15:01:22 -0400

    complete add to order post new + update to db

--- PurchaseMeNow/Areas/Client/Controllers/ProductController.cs
+++ PurchaseMeNow/Areas/Client/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
 using PurchaseMeNow.Models;
@@ -31,6 +33,54 @@ namespace PurchaseMeNow.Areas.Client.Controllers
 
             return View(order);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult AddToOrder(Order orderObj)
+        {
+            orderObj.Id = 0;
+            if (ModelState.IsValid)
+            {
+                //add to cart
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderObj.ApplicationUserId = claim.Value;
+
+               Order orderFromDb = _unitOfWork.Order.GetFirstOrDefault(u => u.ApplicationUserId ==
+               orderObj.ApplicationUserId && u.ProductId == orderObj.ProductId, includeProperties: "Product");
+
+                if(orderFromDb == null)
+                {
+                    //add order in database for user since none exists
+                    _unitOfWork.Order.Add(orderObj);
+                }
+                else
+                {
+                    orderFromDb.Count += orderObj.Count;
+                    _unitOfWork.Order.Update(orderFromDb);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+            else
+            {
+                //return to view
+                var productFromDb = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == orderObj.ProductId, includeProperties: "Category,Department");
+
+                Order order = new Order()
+                {
+                    Product = productFromDb,
+                    ProductId = productFromDb.Id
+                };
+
+                return View(order);
+            }
+
+            
+        }
+
 
 
         #region API Calls
