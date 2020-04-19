069bdb7107874d0ce14e7da93de93ae5cc28d9d3
shihab <echoshihab@gmail.com>
2020-04-19 15:40:20 -0400

    resend email confirmation at order list if not confirmed

--- PurchaseMeNow/Areas/Client/Controllers/OrderController.cs
+++ PurchaseMeNow/Areas/Client/Controllers/OrderController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
 using PurchaseMeNow.Models.ViewModels;
+using PurchaseMeNow.Utility;
 
 namespace PurchaseMeNow.Areas.Client.Controllers
 {
@@ -46,6 +51,80 @@ namespace PurchaseMeNow.Areas.Client.Controllers
 
 
             return View(OrderListVM);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> IndexPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var user = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to find user!");
+            }
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Increment(int orderId)
+        {
+            var order = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "Product");
+
+            order.Count += 1;
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Decrement(int orderId)
+        {
+            var order = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "Product");
+
+            if(order.Count == 1) //remove item from shopping cart and navlink order count
+            {
+                var orderCount = _unitOfWork.Order.GetAll(u => u.ApplicationUserId == order.ApplicationUserId).ToList().Count();
+                _unitOfWork.Order.Remove(order);
+                HttpContext.Session.SetInt32(SD.ssOrder, orderCount - 1);
+                
+
+            }else
+            {
+                order.Count -= 1;
+            }
+     
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Delete (int orderId)
+        {
+            var order = _unitOfWork.Order.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "Product");
+
+  
+                var orderCount = _unitOfWork.Order.GetAll(u => u.ApplicationUserId == order.ApplicationUserId).ToList().Count();
+                _unitOfWork.Order.Remove(order);
+                HttpContext.Session.SetInt32(SD.ssOrder, orderCount - 1);
+
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
