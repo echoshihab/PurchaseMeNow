4c1032159587f918915383f27c0a810aaff77af0
shihab <echoshihab@gmail.com>
2020-04-21 20:57:44 -0400

    add ordercontroller + index for admins

---
+++ PurchaseMeNow/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PurchaseMeNow.DataAccess.Data.Repository.IRepository;
+using PurchaseMeNow.Models;
+
+namespace PurchaseMeNow.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitofWork;
+
+        public OrderController(IUnitOfWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetOrderList()
+        {
+            IEnumerable<OrderHeader> orderHeaderList;
+
+            orderHeaderList = _unitofWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaderList });
+        }
+
+        #endregion
+    }
+}
